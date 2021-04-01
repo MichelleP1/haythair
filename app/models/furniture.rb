d74eb3c0ea34766bb04f8c4fe53c4cd147c73f9b
Michelle <michellepeters94@gmail.com>
2021-04-01 12:49:25 -0500

    First iteration of filter feature for furniture

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -3,3 +3,14 @@ class Furniture < ApplicationRecord
   # validates :name, presence: true
   has_one_attached :image
 end
+
+def self.filter(filter)
+  if filter
+    f = self.where('LOWER(furniture.title) LIKE ?', "sed%")
+    if f
+      f = self.where('LOWER(furniture.title) LIKE ?', "sed")
+    else
+    end
+  else
+  end
+end
