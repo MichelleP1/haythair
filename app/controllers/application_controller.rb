6be22feb11631340c78b64ab6d39243ebe8afd85
Michelle <michellepeters94@gmail.com>
2021-04-13 18:30:42 -0500

    Add custom user fields relating to address. Add province model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :initialize_session
   helper_method :cart, :get_categories
 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :postal, :address, :city,
+               :province_id)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :postal, :address, :city,
+               :province_id, :current_password)
+    end
+  end
+
   private
 
   def get_categories
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_132545) do
+ActiveRecord::Schema.define(version: 2021_04_13_213312) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -84,6 +84,16 @@ ActiveRecord::Schema.define(version: 2021_04_13_132545) do
     t.index ["category_id"], name: "index_furnitures_on_category_id"
   end
 
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "pst"
+    t.decimal "gst"
+    t.decimal "hst"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -92,11 +102,17 @@ ActiveRecord::Schema.define(version: 2021_04_13_132545) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.string "postal"
+    t.string "city"
+    t.integer "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "furnitures", "categories"
+  add_foreign_key "users", "provinces"
 end
